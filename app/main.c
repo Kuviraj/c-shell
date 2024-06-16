62367d3e0096512cfecf7b6cbfd4f1118c1f40d2
Kuviraj Sandhu <ksan306@aucklanduni.ac.nz>
2024-06-16 19:32:09 +1200

    added breaking by exit 0

--- app/main.c
+++ app/main.c
@@ -11,6 +11,7 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
     input[strcspn(input, "\n")] = 0;
+    if (strcmp(input, "exit 0") == 0) break;
     printf("%s: command not found\n", &input[0]);
   }
   return 0;
