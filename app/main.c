bb817144bc5e262c38b68ee200f13632b0cf5cfb
Kuviraj Sandhu <ksan306@aucklanduni.ac.nz>
2024-06-16 19:23:05 +1200

    handled missing command

--- app/main.c
+++ app/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-   /*Uncomment this block to pass the first stage*/
-   printf("$ ");
-   fflush(stdout);
+  /*Uncomment this block to pass the first stage*/
+  printf("$ ");
+  fflush(stdout);
 
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  input[strcspn(input, "\n")] = 0;
+  printf("%s: command not found", &input[0]);
   return 0;
 }
