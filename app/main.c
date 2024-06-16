ada199b295b15c965915639cf072bd6ec28e8fbd
Kuviraj Sandhu <ksan306@aucklanduni.ac.nz>
2024-06-16 19:27:51 +1200

    implemented repl

--- app/main.c
+++ app/main.c
@@ -3,13 +3,15 @@
 
 int main() {
   /*Uncomment this block to pass the first stage*/
-  printf("$ ");
-  fflush(stdout);
-
   // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  input[strcspn(input, "\n")] = 0;
-  printf("%s: command not found\n", &input[0]);
+  while (1) {
+    printf("$ ");
+    fflush(stdout);
+
+    char input[100];
+    fgets(input, 100, stdin);
+    input[strcspn(input, "\n")] = 0;
+    printf("%s: command not found\n", &input[0]);
+  }
   return 0;
 }
