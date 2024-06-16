956d975b19edc358b955e964069f6399cbbb0ea3
Kuviraj Sandhu <ksan306@aucklanduni.ac.nz>
2024-06-16 19:44:44 +1200

    added echo command

--- app/main.c
+++ app/main.c
@@ -11,8 +11,20 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
     input[strcspn(input, "\n")] = 0;
-    if (strcmp(input, "exit 0") == 0) break;
-    printf("%s: command not found\n", &input[0]);
+    if (strcmp(input, "exit 0") == 0)
+      break;
+    char *p = strtok(input, " ");
+    if (strcmp(p, "echo") == 0) {
+      p = strtok(NULL, " ");
+      while (p != NULL) {
+        printf("%s ", p);
+        p = strtok(NULL, " ");
+      }
+      printf("\n");
+    } else {
+
+      printf("%s: command not found\n", &input[0]);
+    }
   }
   return 0;
 }
