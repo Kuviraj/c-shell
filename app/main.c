1a56904786ee9bdd496367c41c1c2554db726a69
Kuviraj Sandhu <ksan306@aucklanduni.ac.nz>
2024-06-16 20:07:06 +1200

    added type command

--- app/main.c
+++ app/main.c
@@ -11,13 +11,27 @@ int main() {
     char input[100];
     fgets(input, 100, stdin);
     input[strcspn(input, "\n")] = 0;
+    char words[3][10] = {"exit", "echo", "type"};
+
     if (strcmp(input, "exit 0") == 0)
       break;
     char *p = strtok(input, " ");
+
     if (strcmp(p, "echo") == 0) {
       printf("%s\n", input + 5);
+    } else if (strcmp(p, "type") == 0) {
+      int found = 0;
+      p = strtok(NULL, " ");
+      for (int i = 0; i < 3; i++) {
+        if (strcmp(p, words[i]) == 0)
+          found = 1;
+      }
+      if (found) {
+        printf("%s is a shell builtin\n", p);
+      } else {
+        printf("%s: not found\n", p);
+      }
     } else {
-
       printf("%s: command not found\n", &input[0]);
     }
   }
